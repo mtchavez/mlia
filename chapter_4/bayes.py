9bccc307fa88c00b3f1219a4c70b526605dc17a3
Chavez <nailsbrokeandfell@gmail.com>
2013-10-06 21:37:39 -0700

    [Chapter 4] personal ads

--- chapter_4/bayes.py
+++ chapter_4/bayes.py
@@ -132,3 +132,76 @@ def test_spam():
 		if classify(array(word_vector), p0, p1, spam_prob) != class_list[doc_index]:
 			error_count += 1
 	print 'the error rate is: ',float(error_count)/len(test_set)
+
+
+def calc_most_freq(vocab_list, full_text):
+    import operator
+    freq_dict = {}
+    for token in vocab_list:
+		freq_dict[token]=full_text.count(token)
+		sorted_freq = sorted(freq_dict.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sorted_freq[:30]
+
+
+def local_words(feed1, feed0):
+	doc_list = []
+	class_list = []
+	full_text =[]
+	min_len = min(len(feed1['entries']), len(feed0['entries']))
+	for i in range(min_len):
+		word_list = parse_string(feed1['entries'][i]['summary'])
+		doc_list.append(word_list)
+		full_text.extend(word_list)
+		class_list.append(1)
+		word_list = parse_string(feed0['entries'][i]['summary'])
+		doc_list.append(word_list)
+		full_text.extend(word_list)
+		class_list.append(0)
+	
+	vocab_list = create_vocab(doc_list)
+	top30_words = calc_most_freq(vocab_list, full_text)
+	for word_pair in top30_words:
+		if word_pair[0] in vocab_list:
+			vocab_list.remove(word_pair[0])
+    
+	training_set = range(2*min_len)
+	test_set = []
+	for i in range(20):
+		rand_index = int(random.uniform(0,len(training_set)))
+        test_set.append(training_set[rand_index])
+        del(training_set[rand_index])
+    
+	train_mat = []
+	train_classes = []
+	for doc_index in training_set:
+		train_mat.append(bag_of_words_to_list(vocab_list, doc_list[doc_index]))
+		train_classes.append(class_list[doc_index])
+	
+	p0V, p1V, spam_prob = train(array(train_mat),array(train_classes))
+	error_count = 0
+	for doc_index in test_set:
+		word_vector = bag_of_words_to_list(vocab_list, doc_list[doc_index])
+		if classify(array(word_vector), p0V, p1V, spam_prob) != class_list[doc_index]:
+			error_count += 1
+    
+	print 'the error rate is: ', float(error_count)/len(test_set)
+	return vocab_list, p0V, p1V
+
+
+def get_top_words(ny, sf):
+	vocab_list, p0V, p1V = local_words(ny, sf)
+	top_ny=[]
+	top_sf=[]
+	for i in range(len(p0V)):
+		if p0V[i] > -6.0: 
+			top_sf.append((vocab_list[i],p0V[i]))
+        if p1V[i] > -6.0:
+			top_ny.append((vocab_list[i],p1V[i]))
+	sorted_sf = sorted(top_sf, key=lambda pair: pair[1], reverse=True)
+	print "SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**"
+	for item in sorted_sf:
+		print item[0]
+	sorted_ny = sorted(top_ny, key=lambda pair: pair[1], reverse=True)
+	print "NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**"
+	for item in sorted_ny:
+		print item[0]
