b7501e297fe175e16dd9f11aba7d875e32fabc0a
Chavez <nailsbrokeandfell@gmail.com>
2013-10-08 20:48:17 -0700

    [Chapter 5] plot gradient ascent

--- chapter_5/gradient.py
+++ chapter_5/gradient.py
@@ -18,7 +18,7 @@ def sigmoid(x):
 
 def grad_ascent(data, labels):
     data_matrix = mat(data)
-    label_matrix = mat(labels)
+    label_matrix = mat(labels).transpose()
     m, n = shape(data_matrix)
     alpha = 0.001
     cycle_max = 500
@@ -28,3 +28,32 @@ def grad_ascent(data, labels):
         err = (label_matrix - h)
         weights = weights + alpha * data_matrix.transpose() * err
     return weights
+
+
+def plot_best_fit(weights):
+    import matplotlib.pyplot as plot
+    dm, lm = load_data()
+    data_array = array(dm)
+    n = shape(data_array)[0]
+    x1 = []
+    y1 = []
+    x2 = []
+    y2 = []
+    for i in range(n):
+        if int(lm[i]) == 1:
+            x1.append(data_array[i, 1])
+            y1.append(data_array[i, 2])
+        else:
+            x2.append(data_array[i, 1])
+            y2.append(data_array[i, 2])
+    
+    fig = plot.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x1, y1, s=30, c='red', marker='s')
+    ax.scatter(x2, y2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x) / weights[2]
+    ax.plot(x, y)
+    plot.xlabel('X1')
+    plot.ylabel('X2')
+    plot.show()
